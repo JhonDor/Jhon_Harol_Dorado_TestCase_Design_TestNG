befcc050a0fc56c21ac771fc86aa728450c5c428
JhonDor <90356685+JhonDor@users.noreply.github.com>
2022-11-27 17:29:24 -0500

    Update DataProvider.java

--- src/test/java/org/espn/utils/DataProvider.java
+++ src/test/java/org/espn/utils/DataProvider.java
@@ -3,6 +3,6 @@ package org.espn.utils;
 public class DataProvider {
     @org.testng.annotations.DataProvider(name = "loginCredentials")
     public static Object[][] inputData() {
-        return new Object[][]{{"test1037@gmail.com", "123espn"}};
+        return new Object[][]{{"test1038@gmail.com", "123espn"}};
     }
 }
